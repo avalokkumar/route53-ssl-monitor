6736a097d105323e9a8a6831fcee93460bbfcc91
“Alok <alok.vishwakarma@freshworks.com>
2022-01-16 23:01:02 +0530

    route53 ssl monitor changes initial commit

--- src/main/java/com/clay/route53sslmonitor/rest/Route53SSLMonitorController.java
+++ src/main/java/com/clay/route53sslmonitor/rest/Route53SSLMonitorController.java
@@ -1,6 +1,11 @@
 package com.clay.route53sslmonitor.rest;
 
+import com.clay.route53sslmonitor.model.ExpirationResult;
+import com.clay.route53sslmonitor.service.DomainServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +17,14 @@ public class Route53SSLMonitorController {
         return "Greetings from Route53 SSL Monitor!";
     }
 
+
+    @Autowired
+    private DomainServiceImpl domainService;
+
+    @GetMapping("/domains/expiry/status")
+    public ExpirationResult getInternalDomains(@RequestParam("pageNumber") String pageNumber, @RequestParam("pageSize") String pageSize) {
+
+        return domainService.getDomainsCertExpiryDetails(Integer.parseInt(pageNumber), Integer.parseInt(pageSize));
+    }
 
 }
